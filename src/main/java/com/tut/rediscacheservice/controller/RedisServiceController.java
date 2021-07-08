eacd740ed122493a6c1722e4eb48effea57a5a58
itumrmo-14 <itumrmo@gmail.com>
2021-07-08 14:05:26 +0200

    Init commit

---
+++ src/main/java/com/tut/rediscacheservice/controller/RedisServiceController.java
@@ -0,0 +1,34 @@
+package com.tut.rediscacheservice.controller;
+
+import com.tut.rediscacheservice.model.Product;
+import com.tut.rediscacheservice.repository.ProductDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/product")
+public class RedisServiceController {
+    @Autowired
+    private ProductDao dao;
+
+    @PostMapping
+    public Product save(@RequestBody Product product) {
+        return dao.save(product);
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return dao.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product findProduct(@PathVariable int id) {
+        return dao.findProductById(id);
+    }
+    @DeleteMapping("/{id}")
+    public String remove(@PathVariable int id)   {
+        return dao.deleteProduct(id);
+    }
+}
---
+++ src/test/java/com/tut/rediscacheservice/RedisCacheServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tut.rediscacheservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisCacheServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
